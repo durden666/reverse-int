6b13c4223752a1d6c99122c8684f8ef7ab3ba887
durden666 <thanks.vlad12@gmail.com>
2020-10-01 14:36:31 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+
+  if (n === undefined) {
+    return false
+  } else {
+    return parseInt(n.toString().split('').reverse().join(''));        
+    }
+  } 
+
